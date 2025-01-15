9736d2a21996df9da2de3cfdb5e031087bd031e3
rober gh <121312118+rover-aficionado@users.noreply.github.com>
2025-01-15 16:08:01 +0100

    prueba

---
+++ advertencia.js
@@ -0,0 +1,3 @@
+// este es un archivo de programas temporal que seá usado mientras la web no está terminada
+
+alert("Esta es una web en periodo de prueba, no está terminada. Disculpe los posibles errores que tenga. Gracias ;)")
